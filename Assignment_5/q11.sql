d950355697476f3e899e6d42ae230b49aa841501
dashutosh04 <ashutoshdas2004@gmail.com>
2025-02-03 11:58:02 +0530

    💫 | UPDATE

--- Assignment_5/q11.sql
+++ Assignment_5/q11.sql
@@ -1,12 +1,11 @@
--- Get names of students, who received final grade ‘F’ in Winter 2003.
+-- Get names of students, who received final grade ‘F’.
 USE clg_std;
 
 SELECT
-    CONCAT (s.First, " ", s.Last) Name
+    CONCAT (s.First, " ", s.Last) Name,
+    r.final
 from
-    student s;
-
-SELECT
-    *
-FROM
-    REGISTRATION;
+    student s
+    LEFT JOIN REGISTRATION r ON s.StudentId = r.StudentId
+WHERE
+    r.final = 'F';
